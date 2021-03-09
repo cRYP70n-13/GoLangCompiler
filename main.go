59cbee8de899256e33ce78a185778ea2aed4145d
cRYP70n-13 <otmane.kimdil@gmail.com>
2021-03-09 19:28:09 +0100

    Finishing the lexer its time to start writing our parser for the cRYP70n programming language

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/goCompiler/repl"
+)
 
 func main() {
-	fmt.Println("Hello from the parser")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is cRYP70N's programming language!\n", user.Username)
+	fmt.Printf("Feel free to type in your code!\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
---
+++ repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/goCompiler/lexer"
+	"github.com/goCompiler/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		// In the case of ctrl + c giving an Interrupt signal
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Fprintf(out, "%+v\n", tok)
+		}
+	}
+}
